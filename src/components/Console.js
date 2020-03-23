69702c9b17d54b075fa82138d53ee7ed26137777
Sean Man <sean8100@gmail.com>
2020-03-23 18:17:29 +0200

    Add basic dark-scrollbar design

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import '../assets/scrollbar.css'
+
 export class Console extends React.Component {
     getDate() {
         const date = new Date()
@@ -32,7 +34,7 @@ export class Console extends React.Component {
         return (
             <div className="absolute inset-0 console h-full min-h-0 flex-grow-0 flex flex-col">
                 <h1 className="font-bold mb-2 border-b 2 border-apple-400">Python Console:</h1>
-                <div className="overflow-auto h-full max-h-full flex flex-col flex-grow-0">
+                <div className="overflow-auto h-full max-h-full flex flex-col flex-grow-0 scrollable">
                     {this.renderConsole()}
                 </div>
             </div>
